f7fd20b758616ad89b47a69bbf899dbf42cb7370
Eke Ferdinand <eke.benedictwitto@gmail.com>
2019-05-08 01:56:33 -0700

    Piggy Bank Java Swing application using embedded database sqlite

---
+++ src/javaconnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ferdinand
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+    Connection conn=null;
+    
+    
+    
+    public static Connection ConnecrDb(){
+    try{
+        Class.forName("org.sqlite.JDBC");
+        Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Ferdinand\\Documents\\NetBeansProjects\\Banking_Management_System\\Banking.sqlite");
+        return conn;
+}catch(Exception e){
+    JOptionPane.showMessageDialog(null, e);
+}
+    return null;
+
+}
+    
+}
+
+
